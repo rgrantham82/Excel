b46c23ef9cd63da74522f2d6b5de0acb1b1019bc
Robert Grantham <robertgrantham40@gmail.com>
2024-08-05 09:38:03 -0500

    Update Udacity_SQL_code.sql

--- Project1/Udacity_SQL_code.sql
+++ Project1/Udacity_SQL_code.sql
@@ -1,23 +1,24 @@
--- The results were analyzed according to availability. In other words, I analyzed trends between 1820 and 2013 which were the only available years based on the queries.
--- This was a simple project in which I analyzed weather patterns between my local area (Austin, TX) and the globe to check for any particular or interesting trends. It involved extracting and loading the necessary data using SQL and then analyzing the data in Excel or Google Sheets. To start, I extracted the required data using the following queries:
+-- Analyze trends in temperature data between 1820 and 2013, the available years in the dataset.
+-- This project involves comparing weather patterns between Austin, TX, and the global average, as well as with another city, San Francisco.
+
+-- Step 1: Extract data for Austin and global average temperatures
+-- This query extracts the average temperature for each year from the global data and Austin city data.
 SELECT g.year,
        g.avg_temp AS global_avg_temp,
        c.avg_temp AS city_avg_temp
 FROM   global_data AS g
-       left join city_data AS c
+       LEFT JOIN city_data AS c
               ON g.year = c.year
 WHERE  c.city = 'Austin'; 
 
--- I went one step further and extracted data for San Francisco to compare between both Austin and the globe with:
+-- Step 2: Extract data for Austin, San Francisco, and global average temperatures
+-- This query compares average temperatures between Austin, San Francisco, and the global average for each year.
 SELECT a.year,
-       a.avg_temp gl_temp,
-       b.avg_temp austin_temp,
-       c.avg_temp sf_temp
-FROM   global_data a
-       left join city_data b
-              ON a.year = b.year
-       left join city_data c
-              ON a.year = c.year
-WHERE  b.city = 'Austin'
-       AND c.city = 'San Francisco';
-
+       a.avg_temp AS global_avg_temp,
+       b.avg_temp AS austin_avg_temp,
+       c.avg_temp AS sf_avg_temp
+FROM   global_data AS a
+       LEFT JOIN city_data AS b
+              ON a.year = b.year AND b.city = 'Austin'
+       LEFT JOIN city_data AS c
+              ON a.year = c.year AND c.city = 'San Francisco';
